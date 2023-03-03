f9dc911c2bf6769f47214f36bd8dafbe0b264b01
tomasbond02 <bondtomy@gmail.com>
2023-03-03 15:23:56 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.message import EmailMessage
+
+def enviarMail():
+    
+    remitente = "tbond@uade.edu.ar"
+    destinatario = "tbond@uade.edu.ar"
+    asunto = "Mail html enviado desde python"
+    mensaje = EmailMessage()
+    
+    mensaje['Subject'] = asunto
+    mensaje['From'] = remitente
+    mensaje['To'] = destinatario
+        
+    mensaje.set_content("hola estoy mandando esto desde python!")
+
+
+    mail_smtp = "mail.uade.com"
+    server = smtplib.SMTP(mail_smtp, '25')
+    
+    server.ehlo()
+    server.starttls()
+    
+    mail_envio = "tbond@uade.edu.ar"
+    mail_contraseña = "tomasbond07"
+    
+    server.login(mail_envio, mail_contraseña)
+    
+    server.send_message(mensaje)
+    
+    server.quit()
+    
+    
+enviarMail()
